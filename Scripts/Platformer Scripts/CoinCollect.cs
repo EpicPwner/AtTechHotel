6df460d5ff50eabfc9a57e1c9c6dac5d15b925c8
Pedro Torres <kkatamari@hotmail.com>
2018-09-27 21:15:21 +0100

    Add scripts
    Add scripts to script repository from a platformer game

---
+++ Scripts/Platformer Scripts/CoinCollect.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CoinCollect : MonoBehaviour {
+
+    private SpriteRenderer _CoinRender;
+    
+    public int CoinCounter;
+    private int dropProb;
+    private static int TotalDrop;
+    public static int coinPurse=0;
+    public static bool _OnOrOff = true;
+
+    // Use this for initialization
+    void Start () {
+
+        _CoinRender = GetComponent<SpriteRenderer>();
+        _CoinRender.enabled = true;
+       
+        
+    }
+	
+	// Update is called once per frame
+	void Update () {
+   
+
+        _OnOrOff = _CoinRender.enabled;
+
+    }
+
+    public void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (_CoinRender.enabled)
+        {
+            dropProb = Random.Range(0, 100);
+            if (dropProb > 0 && dropProb < 70)
+            {
+                TotalDrop = Random.Range(1, 70);
+            }
+            else if (dropProb >= 70 && dropProb < 90)
+            {
+                TotalDrop = Random.Range(71, 250);
+            }
+            else if (dropProb >= 90 && dropProb < 97)
+            {
+                TotalDrop = Random.Range(251, 500);
+            }
+            else if (dropProb >= 97 && dropProb < 100)
+            {
+                TotalDrop = Random.Range(251, 1000);
+            }  
+        coinPurse += TotalDrop;
+        Debug.Log("Coins Dropped: " + TotalDrop);
+        
+        }
+        _CoinRender.enabled = false;
+        
+    }
+
+
+
+}
---
+++ Scripts/Platformer Scripts/UIController.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using UnityEngine.UI;
+using UnityEngine;
+
+public class UIController : MonoBehaviour {
+
+    [Header("Linkage")]
+    [SerializeField] private Text DeathCounter;
+    [SerializeField] private Text CoinCounter;
+    [SerializeField] private PopUp popUp;
+
+    private int CoinDrop;
+    private int Wallet;
+    private CoinCollect CoinCollider;
+
+
+	// Use this for initialization
+	void Start () {
+
+        popUp.Close();
+        
+        
+    }
+	
+	// Update is called once per frame
+	void Update () {
+
+        
+
+    }
+
+    public void OnOpenSettings()
+    {
+        popUp.Open();
+    }
+
+
+
+    public void WalletUpdate()
+    {
+        CoinDrop = CoinCollect.coinPurse;
+
+        if (!CoinCollect._OnOrOff)
+        {
+            Wallet += CoinDrop;
+            CoinCounter.text = Wallet.ToString();
+            Debug.Log(Wallet);
+            CoinCollect._OnOrOff = true;
+            CoinDrop = 0;
+            
+        }
+        
+
+    }
+
+
+}
